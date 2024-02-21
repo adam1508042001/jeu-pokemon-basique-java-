1f7d5e7ba513d58c053b38756420f842428d520f
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-02-21 11:18:41 +0100

    creation de fichier main et un test d'affichage

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        affichage();
+    }
+
+    public static void affichage() {
+        System.out.println("salut c'est la fonction systemeoutprint ");
+        System.out.println("salut c'est la println");
+    }
+}
