a64e47e0dce8e1654cb9775697780fc6e2dc10a6
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-02-21 12:10:00 +0100

    initialisation du pokemon ravuus

---
+++ Ravuus.java
@@ -0,0 +1,13 @@
+public class Ravuus extends Pokemon  {
+
+
+
+    public Ravuus (String nom, String type, Integer point_vie, String cri, String attaque)
+    {
+        //initialisation de ravuuuuuuuus
+        super(nom = "RAVUUS" , type = "humain" , point_vie = 100, cri ="Ravuuuuuuuuuus", attaque = "stream");
+    }
+
+
+
+}
